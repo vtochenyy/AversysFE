143c474805058dadf76f16fafaeffa68270b56c0
Vitaly Tochenyy <Antisvinn@yadnex.by>
2022-11-12 23:53:50 +0300

    antd color chage tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Button, ConfigProvider } from 'antd';
 import './App.css';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<Button style={{ margin: '20px' }} type='primary'>
+				dsd
+			</Button>
+			<Button style={{ margin: '20px' }} type='default'>
+				dsd
+			</Button>
+			<Button style={{ margin: '20px' }} type='dashed'>
+				dsd
+			</Button>
+			<Button style={{ margin: '20px' }} type='ghost'>
+				dsd
+			</Button>
+		</div>
+	);
 }
 
 export default App;
