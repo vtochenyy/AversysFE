2d21b431e43fd4101ab165aa13b8f6c576825b94
vlad <sp0ngbik@gmail.com>
2022-11-22 22:53:14 +0300

    404 page

--- src/components/infoRoadMap/InfoRoadMap.js
+++ src/components/infoRoadMap/InfoRoadMap.js
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import App from "../../App";
 import AutorizedPage from "../../pages/autorizedPage/Autorized";
 import MainPage from "../../pages/mainPage/MainPage";
+import Page404 from "../../pages/page404/Page404";
 import RegisterPage from "../../pages/register/RegisterPage";
 
 const InfoRoadMap = () => {
   return (
     <Routes>
+      <Route path="*" element={<Page404 />} />
       <Route path="/" element={<App />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/autorized" element={<AutorizedPage />} />
---
+++ src/pages/page404/Page404.js
@@ -0,0 +1,5 @@
+const Page404 = () => {
+  return <div>hello i'm 404</div>;
+};
+
+export default Page404;
