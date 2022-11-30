181a86c7aef43283af832f2c90924bc968705398
vlad <sp0ngbik@gmail.com>
2022-11-30 22:52:50 +0300

    check for axios autrhorized

--- src/pages/autorizedPage/Autorized.js
+++ src/pages/autorizedPage/Autorized.js
@@ -1,5 +1,26 @@
+import { Button, Form, Input } from "antd";
+import axios from "axios";
+
 const AutorizedPage = () => {
-  return <div>authorized</div>;
+  return (
+    <div>
+      <h1>AUTORIZED</h1>
+      <Form
+        onFinish={(loginData) => {
+          console.log(loginData);
+          axios.post("http://localhost:9876/users/login", loginData);
+        }}
+      >
+        <Form.Item name="login">
+          <Input placeholder="Введите логин" />
+        </Form.Item>
+        <Form.Item name="password">
+          <Input placeholder="Введите пароль" />
+        </Form.Item>
+        <Button htmlType="submit">Вход</Button>
+      </Form>
+    </div>
+  );
 };
 
 export default AutorizedPage;
